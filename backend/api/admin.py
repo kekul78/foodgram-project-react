19fb149fef129a749e090eb4b72cbbab0f152274
kekul <S-HENTAY-S@yandex.ru>
2024-03-12 22:28:21 +0300

    Add tags

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,18 +1,1 @@
-from django.contrib import admin
 
-from recipes.models import Tag, Ingredient, Recipe, RecipeIngredient
-
-
-class RecepIngredientsInLine(admin.TabularInline):
-    model = RecipeIngredient
-    extra = 1
-
-
-@admin.register(Tag)
-class BlogAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Recipe, Ingredient)
-class RecepiesAdmin(admin.ModelAdmin):
-    inlines = (RecepIngredientsInLine,)
